55c8750d0509cbf6d2b1dc8bb97ddc7ef76d8afa
DinoDafor <dinodafor@mail.ru>
2022-05-02 18:27:57 +0300

    Fixed MissionWaypoint for others devices

--- AIGameUnity/Assets/Scripts/TasksScripts/MissionWaypoint.cs
+++ AIGameUnity/Assets/Scripts/TasksScripts/MissionWaypoint.cs
@@ -9,52 +9,47 @@ public class MissionWaypoint : MonoBehaviour
     public Image img;
     // The target (location, enemy, etc..)
     public Transform target;
-    // UI Text to display the distance
-  //  public Text meter;
     // To adjust the position of the icon
     public Vector3 offset;
-
-    public Camera camera;
-
+    private Camera _camera;
     private void Update()
     {
+       _camera = GameInfo.currentDevice.GetComponentInChildren<Camera>();
         // Giving limits to the icon so it sticks on the screen
         // Below calculations witht the assumption that the icon anchor point is in the middle
         // Minimum X position: half of the icon width
         float minX = img.GetPixelAdjustedRect().width / 2;
         
-        print("MinX: " +  minX);
         // Maximum X position: screen width - half of the icon width
         float maxX = Screen.width - minX;
-        print("maxX: " +  maxX);
         // Minimum Y position: half of the height
         float minY = img.GetPixelAdjustedRect().height / 2;
-        print("MinY: " +  minY);
         // Maximum Y position: screen height - half of the icon height
         float maxY = Screen.height - minY;
-        print("maxY: " +  maxY);
         
 
         // Temporary variable to store the converted position from 3D world point to 2D screen point
-        //GameInfo.currentDevice.GetComponentInChildren<Camera>()
-     //   Vector2 pos = camera.WorldToScreenPoint(target.position + offset);
         Vector2 pos =  GameInfo.currentDevice.GetComponentInChildren<Camera>().WorldToScreenPoint(target.position + offset);
 
+        
+            if(Vector3.Dot((target.position - _camera.transform.position), _camera.transform.forward) < 0)
+            {
+                // Check if the target is on the left side of the screen
+                if(pos.x < Screen.width / 2)
+                {
+                    // Place it on the right (Since it's behind the player, it's the opposite)
+                    pos.x = maxX;
+                }
+                else
+                {
+                    // Place it on the left side
+                    pos.x = minX;
+                }
+            }
+        
+       
         // Check if the target is behind us, to only show the icon once the target is in front
-        if(Vector3.Dot((target.position - transform.position), transform.forward) < 0)
-        {
-            // Check if the target is on the left side of the screen
-            if(pos.x < Screen.width / 2)
-            {
-                // Place it on the right (Since it's behind the player, it's the opposite)
-                pos.x = maxX;
-            }
-            else
-            {
-                // Place it on the left side
-                pos.x = minX;
-            }
-        }
+       
 
         // Limit the X and Y positions
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
@@ -62,7 +57,6 @@ public class MissionWaypoint : MonoBehaviour
 
         // Update the marker's position
         img.transform.position = pos;
-        // Change the meter text to the distance with the meter unit 'm'
-      //  meter.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
+        
     }
 }
